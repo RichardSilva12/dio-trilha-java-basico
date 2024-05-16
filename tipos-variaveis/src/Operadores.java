67934317db9d8b9078c426c1b81ff1eb9b7fdddd
Richard <richard.a.peghin@gmail.com>
2024-05-16 15:51:37 -0300

    segundo

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        String nomeComplreto = "Linguagem " + "Java";
+
+        System.out.println(nomeComplreto);
+    }
+
+}
